486d501d8936b2641d08953e18fcb7be2c05b02b
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-09 14:51:49 -0600

    variables notes

---
+++ variables_notes.py
@@ -0,0 +1,24 @@
+#XR, Variable Notes <= for people to read, not the computer
+
+name = input("Tell me your name: ")
+
+# step 1: create a variable
+# step 2: keyword input
+# step 3: parenthesis
+# step 4: ask user our question
+
+#name = "Leticia"
+
+#1 initialize the variable by nameing it
+#2 assignment operator
+#3 We declare our variable by giving it a value
+
+print(name)
+print("CSP is the best class ever!!")
+print("Pink classroom is the best!")
+print("Hello",name, "welcome to class!")
+
+#1 keyword print
+#2 put parenthesis 
+#3 tell computer what to print (inside of the parenthesis)
+
