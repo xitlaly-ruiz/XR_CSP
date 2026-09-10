199a5fc527f80caf96c5d5ee02a287ce6bd44d4a
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-10 14:50:57 -0600

    variables practice

--- variables_practice.py
+++ variables_practice.py
@@ -1,16 +1,23 @@
 # XR, variable practice
 
-myname = ("Xitlaly")
-breakfast = ("waffles")
-favcolor = ("purple")
-schoolname = ("Shoreline Middle School")
-myeyecolor = ("brown")
-someonesage = ("14")
-favsubject = ("math")
-numberonetoten = ("5")
-number1to100 = ("66")
+myname = "Xitlaly"
+breakfast = "waffles"
+favcolor = "purple"
+schoolname = "Shoreline Middle School"
+theyear = "2026"
+myeyecolor = "brown"
+someonesage = "14"
+favsubject = "math"
+numberonetoten = "5"
+number100to1000 = "250"
 
 print("My name is", myname)
 print("I ate", breakfast, "for breakfast")
 print("a number from 1-10 is", numberonetoten)
-print("")
+print("a number between 1-100 is", number100to1000)
+print("My favorite color is", favcolor)
+print("My previous school was", schoolname)
+print("The year is", theyear)
+print("My eyes are", myeyecolor)
+print("My age is", someonesage)
+print("My favorite subject is", favsubject)
