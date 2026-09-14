b5980de0266a7d128215fa44eda0c20521c9bcb5
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-14 14:48:14 -0600

    memory diagram

---
+++ memory_diagram.py
@@ -0,0 +1,20 @@
+#XR, Memory Diagram activity
+
+name = "Alex"
+print(f"The variable name has the value of {name} and is saved at {id(name)}")
+age = 14
+print(f"The varible age has the value of {age} and is saved at {id(age)}")
+score = 10
+print(f"The variable score has the value of {score} and is saved at {id(score)}")
+score = 25
+print(f"The variable score has the value of {score} and is saved at {id(score)}")
+height = 5.9
+print(f"The variable height has the value of {height} and is saved at {id(height)}")
+city = "Denver" 
+print(f"The variable city has the value of {city} and is saved at {id(city)}")
+zip_code = 80202
+print(f"The variable zip_code has the value of {zip_code} and is saved at {id(zip_code)}")
+price1 = 10
+print(f"The variable price1 has the value of {price1} and is saved at {id(price1)}")
+price2 = 100
+print(f"The variable price2 has the value of {price2} and is saved at {id(price2)}")
