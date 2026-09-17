0dc0fa102bc13a5b15f6493f3c11e8ec0ffd8c78
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-17 14:01:28 -0600

    expressions

---
+++ expressions.py
@@ -0,0 +1,32 @@
+#XR, 7th Integers, Floats, and Expression notes
+
+# Integer => a whole number
+people = 24
+cars = 50
+computers = 29
+awareness = -12
+
+# Float => numbers with decimals
+pi = 3.1415
+temp = -95.6
+cost = 1.99
+rain = 2.17
+
+#Arithmetic Operators(+,-,*,/,//,**, % <= remainder)
+print(f"18/4 is {18/4} or {18//5} with a remainder of {18%4}")
+print(f"18/5 is {18/5} or {18//5} with a remainder of {18%5}")
+
+# order of operations
+grades = [85,66,94,72]
+students = len(grades)
+average = sum(grades)/students
+
+print(f"The average is {int(average)}")
+
+# Convert the data type
+price = float(input("How much did the item cost: "))
+tax = 0.0485
+sales_tax = price * tax
+total = price + sales_tax
+print(f"Your total is {total}")
+
