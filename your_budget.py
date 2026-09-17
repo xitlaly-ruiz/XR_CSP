cdca4ad34c2deccb6875e63acb91ea20544e36da
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-17 14:52:45 -0600

    your budget

---
+++ your_budget.py
@@ -0,0 +1,39 @@
+#XR, Your Budget
+
+while True: 
+    try:
+        income = float(input("what is your monthly income?: "))
+        break
+    except:
+        print("This is not what i asked for")
+
+while True: 
+    try:
+        rent = float(input("what is your monthly rent/mortgage?: "))
+        break
+    except:
+        print("This is not what i asked for")
+
+while True: 
+    try:
+        utilities= float(input("what is your monthly utilities?: "))
+        break
+    except:
+        print("This is not what i asked for")
+
+while True: 
+    try:
+        groceries= float(input("what is your monthly groceries?: "))
+        break
+    except:
+        print("This is not what i asked for")
+
+while True: 
+    try:
+        transportation = float(input("what is your monthly transportion?: "))
+        break
+    except:
+        print("This is not what i asked for")
+    
+
+    
