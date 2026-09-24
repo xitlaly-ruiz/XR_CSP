dbe94b50033fc0a4a0c4cfcc953beb5713b38fc3
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-24 14:15:23 -0600

    passwords

--- passwords.py
+++ passwords.py
@@ -1,8 +1,33 @@
 #XR, Password Strength Checker
 
-password = input("What is your password: ")
-charecters = False
+length = False
 uppercase = False
 lowercase = False
 number = False
 symbol = False 
+
+password = input("What is your password: ")
+
+for letter in password:
+    if len(password) >= 8:
+        length = True
+
+for letter in password:
+    if letter .isupper():
+        uppercase = True
+
+for letter in password: 
+    if letter .islower():
+        lowercase = True
+
+for letter in password:
+    if letter .isnumeric():
+        number = True
+
+if letter in "+?#$%!^&*()":
+    symbol = True
+
+
+
+
+ 
