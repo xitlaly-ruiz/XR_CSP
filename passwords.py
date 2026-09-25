c0601653ac01630530aaf9562555a0624a92ad0e
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-25 03:27:25 +0000

    passwords

--- passwords.py
+++ passwords.py
@@ -5,6 +5,8 @@ uppercase = False
 lowercase = False
 number = False
 symbol = False 
+count = 0
+strength = "Weak"
 
 password = input("What is your password: ")
 
@@ -24,10 +26,31 @@ for letter in password:
     if letter .isnumeric():
         number = True
 
-if letter in "+?#$%!^&*()":
+if letter in ".+?#$%!^&*()":
     symbol = True
+print(f"Has a symbol: {symbol}")
 
+if length == True:
+    count = count + 1 
+if uppercase == True:
+    count = count + 1
+if lowercase == True:
+    count = count + 1 
+if number == True:
+    count = count + 1 
+if symbol == True:
+    count = count + 1 
 
+if count == 5:
+    strength = "Strong"
+if count <= 4:
+    strength == "Medium"
+if count < 3:
+    strength = "Weak"
+
+print(f"You have a {strength} password.")
+print(f"You have done {count}/5")
+print(f"If you dont have a 5/5, you should check if you have a uppercase letter, lowercase letter, a number, and a symbol.")
 
 
  
