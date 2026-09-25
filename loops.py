bff309e8c3324b9dd180e6f1b9dc423dc5eaf29d
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-25 14:31:59 -0600

    loops

--- loops.py
+++ loops.py
@@ -26,5 +26,36 @@ print("GOOSE!!!!")
 # must be valid data type
 # bracktes
 
-sibilings = ["Cortez", "Galilea", "Nazareth", "Leticia", "Esperanza"] #<= surround by brackets
+sibilings = ["Galilea", "Nazareth", "Leticia", "Esperanza"] #<= surround by brackets
 print(sibilings[2])
+#adding to a list
+sibilings.append("Yaneli") # <= adds the item to the end of the list
+
+sibilings.insert(3, "Xitlaly")
+
+print(sibilings)
+#remove from a list
+sibilings.pop(3) # <= if no number given pop removes the last item
+print(sibilings)
+
+#print each item in a list
+for sibling in sibilings:
+    print(sibilings)
+
+# For loops
+for num in range(1,25): #range builds a list for you (start, stop, counting by)
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+#for temp in temperature, keyword to start loop is => for
+#temp, variable for current iteration
+#in, keyword for loop
+#temperature, list were looking at
+
+
