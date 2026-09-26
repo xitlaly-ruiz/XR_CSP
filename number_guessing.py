923da50d177c0465059b5ab2ea80257ac08f09bb
xitlaly-ruiz <xitlaly.ruiz@ucas-edu.net>
2026-09-26 01:48:42 +0000

    numberguess

--- number_guessing.py
+++ number_guessing.py
@@ -1,8 +1,25 @@
-#XR, Number Guessing Game
+# XR, Number Guessing Game
 import random
 
-guess = 0
-answer = random.randit(1,101)
+answer = random.randint(1, 100)
+guesses = 0
+
+while guesses < 6:
+	guess = int(input("Guess a number from 1 to 100: "))
+	guesses = guesses + 1
+
+	if guess == answer:
+		print(f"Correct you got it in {guesses} tries!")
+		break
+	elif guess < answer:
+		print("Guess higher")
+	else:
+		print("Guess lower")
+
+if guess != answer:
+	print(f"Your out of tries. The answer was {answer}")
+
+
 
 
 
